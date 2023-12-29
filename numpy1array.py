1dbdc99cc478fcdc7f39dc0fcdce02632649104e
Nandakishor.V <nandakishor.v2020@vitbhopal.ac.in>
2023-12-29 15:27:26 +0530

    g

--- numpy1array.py
+++ numpy1array.py
@@ -8,7 +8,22 @@ print(b)
 
 c=np.array([[[45,3,5],[5,7,2]],[[5,6,7],[4,7,8]]])
 print(c)
-
+'''
 print(a[1:3:2])
 
 print(b[1,1])
+
+print(a.dtype)
+d=a.copy()
+a[0]=34
+e=a.view()
+print(a)
+print(d)
+print(e) '''
+
+
+print(a.shape)
+print(a.size)
+print(b.shape)
+print(b.reshape(3,2,1))
+print(b.size)
