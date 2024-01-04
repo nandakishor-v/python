5d2e2884d2a90ec70aa04318c368c8de054b6af4
Nandakishor.V <nandakishor.v2020@vitbhopal.ac.in>
2024-01-04 11:46:32 +0530

    cc

--- bargraph1.py
+++ bargraph1.py
@@ -17,5 +17,19 @@ ply.pie(y,labels=x,shadow=True,explode=[0.1,0,0,0],autopct="%1.1f%%")
 ply.legend(title="footballers",loc=(-0.7,0.1))
 
 
+data=[]
+for x in range(100):
+          data.append(np.random.rand())
+
+
+ply.subplot(2,2,4)
+ply.hist(data,color="red")
+
 ply.show()
 
+print(np.mean(y))
+print(np.median(y))
+
+print(np.var(data))
+print(np.std(data))
+print(np.percentile(data,1))
