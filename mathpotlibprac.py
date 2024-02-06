8e04c282969d53b30809cf6e09de3cc75ba227bb
Nandakishor.V <nandakishor.v2020@vitbhopal.ac.in>
2024-02-06 11:46:13 +0530

    f

--- mathpotlibprac.py
+++ mathpotlibprac.py
@@ -1,24 +1,20 @@
-# import required modules 
-import matplotlib.pyplot as plt 
-import numpy as np 
-	
-# time series data 
-geeksx = np.array([24.40, 110.25, 20.05, 
-				22.00, 61.90, 7.80, 
-				15.00]) 
+# importing module
+import matplotlib.pyplot as plt
 
-geeksy = np.array([24.40, 110.25, 20.05, 
-				22.00, 61.90, 7.80, 
-				15.00]) 
-	
-# depict illustration 
-fig, ax = plt.subplots() 
-ax.xcorr(geeksx, geeksy, maxlags = 6, 
-		color ="green") 
+# assigning x and y coordinates
+x = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
+y = []
+
+for i in range(len(x)):
+	y.append(max(0, x[i]))
+
+# depicting the visualization
+ax = plt.plot(x, y, color='green')
 
 # turn off the axes
-ax.set_axis_off() 
+#plt.axis('off')
 
-# assign title
-ax.set_title('Time series graph') 
-plt.show() 
+# displaying the title
+plt.title("ReLU function graph")
+
+plt.show()
